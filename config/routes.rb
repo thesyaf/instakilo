a44162901bfb5b7af74806cd5b202f4d9d53e671
Syaf Djamil <the.syaf@gmail.com>
2017-10-18 22:21:53 +1100

    forced profile creation into sign up flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { registrations: "registrations" }
 
-  devise_for :users
   resources :photos, only: [:show, :new, :create, :index, :edit, :update]
 
   root 'photos#index'
